6da2c45b8180e6b09d2f6262f62cc71a4bde9062
Johan Persson <johan.persson@schibsted.se>
2016-09-23 15:17:21 +0200

    Removed obsolete files.

--- packages/roc-plugin-typescript/src/roc/index.js
+++ packages/roc-plugin-typescript/src/roc/index.js
@@ -1,11 +1,6 @@
-import config from '../config/roc.config.js';
 import builder from '../builder';
 
-import { name } from './util';
-
 export default {
-    name,
-    config,
     actions: {
         typescript: {
             hook: 'build-webpack',
